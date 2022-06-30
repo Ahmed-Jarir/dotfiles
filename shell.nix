edc0e9c4ee35f8df248de19c43133f473d146cb3
AJAREER20 <ahmadjareer@gmail.com>
2022-06-30 19:41:30 +0300

    added fish config, changed fish prompt

--- shell.nix
+++ shell.nix
@@ -12,8 +12,45 @@ in {
     programs.fish = {
         enable = true;
     	inherit shellAliases;
+		shellInit = ''
+			fish_vi_key_bindings
+			set fish_greeting
+            function fish_prompt
 
+            set -l namebg '242F9B'
+            set -l dirTypeBg '646FD4'
+            set -l dirBg '9BA3EB'
+            set -l normalFontColor '000000'
+            set_color white -b $namebg
+
+            printf ' %s ' (whoami)
+            set_color normal
+            set_color $namebg -b $dirTypeBg
+            printf ''
+            set_color $normalFontColor -b $dirTypeBg
+            if [ (prompt_pwd) = '~' ]
+                printf ' ~ '
+                set_color $dirTypeBg -b normal
+                printf ''
+            else if [ (prompt_pwd) = '/' ]
+                printf ' / '
+                set_color $dirTypeBg -b normal
+                printf ''
+            else
+                printf '  '
+                set_color $dirTypeBg -b $dirBg
+                printf ''
+                set_color $normalFontColor -b $dirBg
+                printf ' %s ' (prompt_pwd)
+                set_color $dirBg -b normal
+                printf ''
+            end
+
+            set_color normal
+            end
+        '';
         plugins = [
+
             {
               name = "z";
               src = pkgs.fetchFromGitHub {
