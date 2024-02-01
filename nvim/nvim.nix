7f38e550599e1fcfaab6f1766f5f6ab0bbee6144
Ahmad-Jarir <ahmadjareer@gmail.com>
2024-02-02 01:11:02 +0300

    finished the transition to nixvim

--- nvim/nvim.nix
+++ nvim/nvim.nix
@@ -4,40 +4,90 @@ pkgs:
     colorschemes.onedark.enable = true;
     globals.mapleader = " ";
 
-
-    #extraPlugins = with pkgs.vimPlugins; [
-    
-    #]
     options = {
         number = true;
         relativenumber = true;
-        ignorecase = true;
+
         smartindent = true;
         autoindent = true;
+        backspace = "indent,eol,start";
+        wrap = false;
+        signcolumn = "yes";
+        guicursor = "i:block";
+
         tabstop = 4;
         shiftwidth = 4;
         expandtab = true;
+
         termguicolors = true;
+        title = true; 
+
         clipboard = "unnamedplus";
+
         mouse = "a";
+
         smartcase = true;
+
         ttimeoutlen = 5;
+
         compatible = false;
+
         autoread = true;
+
         incsearch = true;
+        ignorecase = true;
+
         hidden = true;
+
         shortmess = "A";
+
+        undofile = true;
+        backup = true;
+        swapfile = false;
+
     };
     plugins = {
         telescope.enable = true;
         treesitter.enable = true;
+        markdown-preview.enable = true;
+        notify.enable = true;
         comment-nvim = {
             enable = true;
             opleader = {
                 line = "<leader>c";
-                block = "<leader>cb";
+                block = "<leader>b";
+            };
+            toggler = {
+                line = "<leader>cc";
+                block = "<leader>bb";
+            };
+
+        };
+        lualine.enable = true;
+        gitsigns.enable = true;
+        lsp = {
+            enable = true;
+            servers = {
+                rnix-lsp.enable = true;
+                pyright.enable = true;
+                ccls.enable = true;
+                lua-ls.enable = true;
+                csharp-ls.enable = true;
             };
         };
+        nvim-cmp = {
+            enable = true;
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+              { name = "buffer"; }
+            ];
+            mapping = {
+                "<tab>" = "cmp.mapping.select_next_item()";
+                "<s-tab>" = "cmp.mapping.select_prev_item()";
+            };
+        };
+        # neogen.enable = true;
     };
     extraConfigLua = ''
         local g = vim.g
@@ -69,58 +119,4 @@ pkgs:
             options.desc = "find buffer";
         }
     ];
-
-
-# nowrap = true;
-# signcolumn = true;
-# noswapfile = true;
-# noerrorbells = true;
-
-  
-  # vimAlias = true;
-  # extraConfig = ''
-  #   luafile ${./init.lua}
-  #   luafile ${./plugins/init.lua}
-  # '';
-  # plugins = with pkgs.vimPlugins; [
-
-  #   vim-nix
-  #   nvim-comment
-  #   telescope-nvim
-
-  #   nvim-lspconfig
-  #   nvim-cmp
-  #   cmp-nvim-lsp
-  #   cmp-nvim-ultisnips
-  #   telescope-ultisnips-nvim
-  #   ultisnips
-
-  #   neogit
-  #   gitsigns-nvim
-  #   vim-kitty-navigator
-  #   markdown-preview-nvim
-
-  #   #eye candy
-  #   onehalf
-  #   dashboard-nvim
-  #   lualine-nvim
-  #   #indent-blankline-nvim
-  #   nvim-web-devicons
-  #   # (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
-  #   (pkgs.fetchFromGitHub {
-  #     owner = "xiyaowong";
-  #     repo = "nvim-transparent";
-  #     rev = "1a3d7d3b7670fecbbfddd3fc999ddea5862ac3c2";
-  #     sha256 = "sha256-ollCztmgulpMTyoks9ENMSmzE52dF9sMXti9ZF1SHnE=";
-  #   })
-  # ];
-  # extraPackages = with pkgs; [
-  #   ripgrep
-  #   fd
-
-  #   lua-language-server
-  #   ccls
-  #   pyright
-  #   rnix-lsp
-  # ];
 }
